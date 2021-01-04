45fc3209b8734376647311e45a1bcd40239a9cc1
Mer Alvin Grita <mer.grita@stratusmeridian.com>
2021-01-04 22:36:14 +0800

    Updates
    1. Integrate Yarn
    2. Integrate Sass
    3. Integrate Bootstrap
    4. Fixed folder structure

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,22 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Site under maintenance</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Home = () => (
+  <div className="page-wrapper">
+    <Head>
+      <title>Site under maintenance</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main>
-        <Header title="Site under maintenance." />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    <main>
+      <Header title="Site under maintenance." />
+      <p className="description">
+        Get started by editing <code>pages/index.js</code>
+      </p>
+    </main>
 
-      <Footer />
-    </div>
-  )
-}
+    <Footer />
+  </div>
+);
+
+export default Home;
