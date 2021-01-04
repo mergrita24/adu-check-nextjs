ff6efbfa2528f35b66127deaa66917eb606f0a83
Mer Alvin Grita <mer.grita@stratusmeridian.com>
2021-01-04 23:12:57 +0800

    updates
    1. added scripts for jquery, popper and bootstrap

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,14 @@ import Footer from '@components/Footer'
 const Home = () => (
   <div className="page-wrapper">
     <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <title>Site under maintenance</title>
       <link rel="icon" href="/favicon.ico" />
+
+      {/* jQuery first, then Popper.js, then Bootstrap JS */}
+      <script src="/scripts/jquery.min.js"></script>
+      <script src="/scripts/popper.min.js"></script>
+      <script src="/scripts/bootstrap.min.js"></script>
     </Head>
 
     <main>
