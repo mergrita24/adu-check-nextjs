bc76547bbe4904a70d0db41d9d5b0175dcceced4
Mer Alvin Grita <mer.grita@stratusmeridian.com>
2021-01-09 11:59:02 +0800

    Updates
    1. Update image scaling of logo.

--- components/Hero_Banner.js
+++ components/Hero_Banner.js
@@ -5,8 +5,8 @@ function Hero_Banner() {
       <section className="adu-hero-banner position-relative d-flex align-items-center">
         
         
-        <div class="vector-container w-100 text-center">
-          <img src="/la-skyline.svg" class="vector-skyline img-fluid" alt="" />
+        <div className="vector-container w-100 text-center">
+          <img src="/la-skyline.svg" className="vector-skyline img-fluid" alt="" />
         </div>
       </section>
     </>
