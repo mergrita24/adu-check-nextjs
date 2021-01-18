870e2ba1f130c69f0c14b312c984f8cda29d4097
Yamini-R <47134997+Yamini-R@users.noreply.github.com>
2021-01-18 19:07:02 +0530

    refactor

--- components/Home_section.js
+++ components/Home_section.js
@@ -1,9 +1,9 @@
 const Home_section = () => (
-  <section className="adu-home--section-1">
+  <section class="adu-home--section-1 d-flex align-items-center">
     <div className="container">
-      <div className="row d-flex mt-md-5 pt-md-5 mt-5">
-        <div className="col-md-6 pt-5">
-          <div className="inner-content--left">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="inner-content--left mt-5 pt-5">
             <h2 className="adu-sec1--what-is-adu">WHAT IS AN ADU?</h2>
             <p className="adu-sec1--an-accessory">
               An accessory dwelling unit (ADU) is a legal and regulatory term
@@ -35,8 +35,8 @@ const Home_section = () => (
           </div>
         </div>
 
-        <div className="col-md-6 mt-md-5 mt-5">
-          <div className="inner-content--right">
+        <div className="col-md-6 mt-5">
+          <div className="inner-content--right mt-5 pt-3">
             <img className="adu-img1" src="/adu_1.svg" />
           </div>
         </div>
